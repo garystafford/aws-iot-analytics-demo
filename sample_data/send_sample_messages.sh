e8592e3b21098ecd5d03d4a54717a6beb85f3295
Gary A. Stafford <garystafford@rochester.rr.com>
2020-07-13 10:26:03 -0400

    Refactor script

--- sample_data/send_sample_messages.sh
+++ sample_data/send_sample_messages.sh
@@ -2,7 +2,7 @@
 
 # Author: Gary A. Stafford
 # Sends sample messages to AWS IoT Core Topic
-# Usage: sh ./send_message.sh raw_data_small.json
+# Usage: sh ./send_sample_messages.sh raw_data_small.json
 
 if [[ $# -ne 1 ]]; then
   echo "Script requires 1 parameter..."
