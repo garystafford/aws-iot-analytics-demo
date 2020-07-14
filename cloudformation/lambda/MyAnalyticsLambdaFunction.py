6f876c25c8ea8f8861f0b07ad3afbddba9c73a05
Gary A. Stafford <garystafford@rochester.rr.com>
2020-07-13 23:13:43 -0400

    Add Lambda

---
+++ cloudformation/lambda/MyAnalyticsLambdaFunction.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+def convert_time(t):
+    return datetime.datetime.utcfromtimestamp(t).isoformat()
+
+
+def lambda_handler(event, context):
+    for e in event:
+        e['temp'] = round(e['temp'], 2)
+        e['humidity'] = round(e['humidity'], 2)
+        e['co'] = round(e['co'], 4)
+        e['lpg'] = round(e['lpg'], 4)
+        e['smoke'] = round(e['smoke'], 4)
+        e['ts'] = convert_time(e['ts'])
+    return event
